361fc83c841422dace310506b05b8bd12f6d09e1
up1 <somkiat.p@gmail.com>
2026-03-26 00:23:30 +0700

    Add goroutine leak

--- workshop/demo-pprof/main.go
+++ workshop/demo-pprof/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	// https://github.com/gin-contrib/pprof
 	"github.com/gin-contrib/pprof"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -39,6 +38,19 @@ func main() {
 		})
 	})
 
+	// Define an endpoint that simulates a Goroutine leak
+	router.GET("/goroutine-leak", func(c *gin.Context) {
+		GoroutineLeak()
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Simulated Goroutine leak",
+		})
+	})
+
+	// Define an endpoint that simulates a panic
+	router.GET("/panic", func(c *gin.Context) {
+		panic("Simulated panic")
+	})
+
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
 	router.Run()
@@ -56,7 +68,18 @@ func MemoryLeak() {
 
 func CPUSpike() {
 	// Simulate a CPU spike by performing a large number of calculations
-	for i := 0; i < 1e7; i++ {
+	for i := range int(1e7) {
 		_ = i * i // Perform some calculation to consume CPU
 	}
+}
+
+func GoroutineLeak() {
+	// Simulate goroutine waits forever, channel is never closed
+	ch := make(chan struct{})
+	go leakyWorker(ch)
+}
+
+func leakyWorker(ch chan struct{}) {
+	// This goroutine blocks forever if channel is never closed
+	<-ch
 }
